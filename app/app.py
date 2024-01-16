0e0f0a4b2264ecc568704c6839031b8988cbefe4
emmariescurrena <emmariescurrena>
2024-01-16 10:21:37 -0300

    jeje

--- app/app.py
+++ app/app.py
@@ -8,6 +8,17 @@ def index(name="Roberto"):
     return render_template("index.html", name=name)
 
 
-@app.route("/temas")
-def tema():
-    return render_template("tema.html")
+@app.route("/topico")
+def topico():
+    return render_template("topico.html")
+
+
+usuario = {
+    "nombre": "david_musteink",
+    "fecha_creacion": "05/06/1998"
+}
+
+
+@app.route("/usuario")
+def usuario(usuario=None):
+    return render_template("usuario.html", usuario=usuario)
