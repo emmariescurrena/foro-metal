8f1289089bf18886ecb2853adab2883be007bc96
emmariescurrena <emmariescurrena>
2024-02-02 08:44:17 -0300

    Puts .gitignore on root

---
+++ tests/unit/test_signup.py
@@ -0,0 +1,12 @@
+"""Unit test for signup functions"""
+
+from src.auth import name_registered, email_registered, valid_email_format, valid_password_format
+import pytest
+
+
+def test_name_registered():
+    pass
+
+
+def test_email_registered():
+    pass
