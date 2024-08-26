5a14f381197b4d602553a9c38f30b4dae71faf7d
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-26 10:32:15 -0300

    Secret key and postgres uri are set from .env

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 """Configuration for app"""
 
-SECRET_KEY = "df9fbc3c0416a14829c1c28f8bf1a93fa968e6f0541e466cf0124df2d8f9acfd"
-SQLALCHEMY_DATABASE_URI = "postgresql://emmanuel:cafetera@localhost:5432/foro_de_metal"
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
